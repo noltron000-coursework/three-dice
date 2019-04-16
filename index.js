9f45a0eec301a7d07e486088280a6b78a13b359b
Nolan Kovacik <noltron000@gmail.com>
2019-04-15 22:22:08 -0700

    moving entrypoint to src

--- index.js
+++
@@ -1,3 +0,0 @@
-exports.printMsg = () => {
-	console.log("This is a message from the demo package");
-}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+exports.printMsg = () => {
+	console.log('This is a message from the demo package')
+}
